d57eefbf8c01d6a59886a859a24bd31533cf34c5
Olga Lapotentova <olgalapotentova@gmail.com>
2015-07-22 16:00:31 +0300

    add new class  in Net01.1 progect

---
+++ Net01_1/Model/SomeNewClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Net01_1.Model
+{
+    class SomeNewClass
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
